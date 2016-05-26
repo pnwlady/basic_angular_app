96e336b9481f8ae7a23bd440a25fab7a0eac9a68
heidilaursen <heidimlaursen@gmail.com>
2016-05-26 14:08:41 -0700

    rolling back server to may 11

--- client_server.js
+++
@@ -1,2 +0,0 @@
-const express = require('express');
-express().use(express.static(__dirname + '/build')).listen(5000, () => console.log('server up'));
--- index.js
+++
@@ -1,2 +0,0 @@
-require(__dirname + '/server.js');
-require(__dirname + '/client_server.js');
