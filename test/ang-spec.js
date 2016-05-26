1ab7c564c08eb10b3730afcd7171b7ce5ac822aa
heidilaursen <heidimlaursen@gmail.com>
2016-05-25 22:20:16 -0700

    adding protractor again

---
+++ test/ang-spec.js
@@ -0,0 +1,9 @@
++describe('our super awesome angular app', function() {
+ +  it('should have a 2 way data binding', () => {
+ +    browser.get('http://localhost:4020');
+ +    element(by.model('greeting')).sendKeys('hello world');
+ +    element(by.id('greeting')).getText().then(function(text) {
+ +      expect(text).toEqual('hello world');
+ +    });
+ +  });
+ +});
