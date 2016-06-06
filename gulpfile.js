b9c28152db7284291c43f507cc8abdc859bbdb93
heidilaursen <heidimlaursen@gmail.com>
2016-06-05 19:00:55 -0700

    fixing tests - running selinium and server from start

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
+const nodemon = require('gulp-nodemon');
+const cp = require('child_process');
+const exec = require('child_process').exec;
+const protractor = require('gulp-protractor').protractor;
+const mongoUri = 'mongodb://localhost/4020';
+var children = [];
 
 gulp.task('webpack:dev', () => {
   gulp.src('./app/js/entry.js')
@@ -21,5 +27,39 @@ gulp.task('css:dev', () => {
     .pipe(gulp.dest('./build'));
 });
 
-gulp.task('default', ['webpack:dev', 'static:dev', 'css:dev']);
-gulp.task('build:dev', ['webpack:dev', 'static:dev', 'css:dev']);
+gulp.task('startservers:test', ['webpack:dev', 'static:dev', 'css:dev'], () => {
+  children.push(cp.fork('server.js'));
+  children.push(cp.spawn('webdriver-manager', ['start']));
+  children.push(cp.spawn('mongod', ['--dbpath=./db']));
+  children.push(cp.fork('../', [], {env: {MONGO_URI: mongoUri}}));
+});
+
+// gulp.task('startServers:develop', ['webpack:dev', 'static:dev', 'css:dev'], () => {
+//   nodemon({
+//     script: 'server.js',
+//     ext: 'html js css',
+//     ignore: ['build'],
+//     tasks: ['protractor']
+//   });
+// });
+
+// gulp.task('startServers:selenium', () => {
+//   exec('webdriver-manager start', (cb) => {
+//     cb(err);
+//   });
+// });
+
+gulp.task('protractor', ['startservers:test'], () => {
+  gulp.src(['./test/*spec.js'])
+    .pipe(protractor({
+        configFile: 'test/config.js',
+        args: ['--baseUrl', 'http:127.0.0.1:5000']
+      }))
+    .on('end', () => {
+      children.forEach((child) => {
+        child.kill('SIGTERM');
+      });
+    });
+});
+
+gulp.task('default', ['protractor']);
